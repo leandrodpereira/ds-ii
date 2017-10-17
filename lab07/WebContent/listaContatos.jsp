63e1c7e3cb259cb348176384db6c07c29bd038af
lduarte <lduarte@lduarte-unifei>
2017-10-17 14:14:42 -0200

    Lab 07

---
+++ lab07/WebContent/listaContatos.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="css/bootstrap.css" rel="stylesheet">
+<title>Lista de Contatos</title>
+</head>
+<body>
+	<jsp:useBean id="cdao" class="br.fepi.si.jdbc.dao.ContatoDao"></jsp:useBean>
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Nome</th>
+				<th>E-mail</th>
+				<th>Endereço</th>
+				<th>Data de Nascimento</th>
+			</tr>
+		</thead>
+		<tbody>
+				<c:forEach var="contatos" items="${cdao.selectAll()}">
+				<tr>
+					<td>${contatos.nome}</td>
+					<td>${contatos.endereco}</td>
+					<td>${contatos.email}</td>
+					<td>${contatos.dataNascimento.time}</td>
+				</tr>
+				</c:forEach>
+		</tbody>
+	</table>
+</body>
+</html>
---
+++ lab07/src/br/fepi/si/jdbc/model/Contato.java
@@ -0,0 +1,78 @@
+package br.fepi.si.jdbc.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * 
+ * @author Leandro
+ *
+ */
+public class Contato {
+	private Long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	private Calendar dataNascimento;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	@Override
+	public String toString() {
+		return "Contato [Id=" + id + ", Nome=" + nome + ", Email=" + email + ", Endereço=" + endereco
+				+ ", Data de Nascimento=" + new SimpleDateFormat("dd/MM/yyyy").format(dataNascimento.getTime()) + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+	
+	
+}
